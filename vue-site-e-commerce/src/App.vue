1621644f9955d07bd53622b75419bcf90396face
Duchoang98 <duchoang2612yh@gmail.com>
2021-10-15 23:21:29 +0200

    Staged files

--- vue-site-e-commerce/src/App.vue
+++ vue-site-e-commerce/src/App.vue
@@ -1,73 +1,24 @@
 <template>
-<div>
-    <Nav :navLinks="navLinks" :navConfig="navConfig" :btnConfig="btnConfig">
-      <img class="img" src="logo" alt="" srcset="" /> <!-- A slot For your project Logo -->
-    </Nav>
+  <div>
+    <Header />
+    <Homepage />
+
   </div>
 </template>
 
 <script>
-import Nav from "vue-nav-ui";
+
+import Header from "./components/Header.vue"
+import Homepage from "./components/Homepage.vue"
 
 export default {
   components: {
-    Nav,
+    Header,
+    Homepage
   },
-  data(){
-        return {
-            /* FOR YOUR NAVIGATION LINKING NAMES AND PATHS */ 
-            navLinks: [
-                {
-                    name: "Home",
-                    path: "/",
-                },
-                {
-                    name: "About",
-                    path: "/about",
-                },
-                {
-                    name: "FAQs",
-                    path: "/faqs",
-                },
-                {
-                    name: "More",
-                    path: "/more",
-                },
-                {
-                    name: "Media",
-                    path: "/media",
-                },
-            ],
-            navConfig: {
-                whitespace: true, /* GIVES PADDING TO YOUR NAV, IF SET TO FALSE, REMOVES PADDING */
-                navBg: "#FAFAFA", /* BACKGROUND COLOR OF YOUR NAV  */
-                navBorderRadius: "30px", /* BORDER RADIUS OF YOUR NAV */
-                linkFont: "poppins", /* FONT FAMILY OF YOUR NAV */
-                linkColor: "black", /* FONT COLOR OF YOUR NAV */
-                responsivePosition: "top or bottom", /* FOR CHANGING THE POSITION OF YOUR NAV WHEN RESPONSIVE. BOTTOM or TOP | The only two options */
-            },
-            btnConfig: {
-                btnLink: true, /* FOR INITIALIZING NAV BUTTON USAGE, IF SET TO FALSE, REMOVES THE NAV BUTTON  */
-                btnUrl: "https://dhaniel.disha.page", /* LINK URL OF YOUR NAV BUTTON */
-                btnText: "Download app", /* NAV BUTTON TEXT */
-                btnBg: "#40269E", /* BACKGROUND COLOR OF YOUR NAV BUTTON  */
-                btnTextColor: "white", /* FONT COLOR OF YOUR NAV BUTTON*/
-                btnBorderWidth: "0", /* BORDER WIDTH OF YOUR NAV BUTTON */
-                btnBorderColor: "black", /* BORDER COLOR OF YOUR NAV BUTTON */
-                btnBorderRadius: "20px", /* BORDER RADIUS OF YOUR NAV BUTTON */
-            }
-        }
-    }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-site-e-commerce/src/components/ComicCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="comic-card">
+      
+    <div class="comic-image">
+
+    </div>
+    <div class="comic-information">
+        
+        <div class="comic-information__top">
+            <p class="name">
+                Solo leveling
+            </p>
+            <p class="note">
+                4.5
+            </p>
+        </div>
+       
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ComicCard",
+};
+</script>
+
+<style scoped>
+.comic-card {
+  width: 20%;
+  height: 60vh;
+}
+
+.comic-image {
+  background-image: url("https://cdn.scanmanga.eu/img/manga/Solo_Leveling_1_4690.jpg");
+  height: 70%;
+  background-size: cover;
+  background-position:initial;
+  cursor: pointer;
+}
+
+.comic-information__top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10px 0;
+}
+.note {
+    font-weight: bold;
+    font-size: 14px;
+    border: 2px solid black;
+    border-radius: 50% ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(255, 0, 128);
+    height: 30px;
+    width: 30px;
+    color: white;
+    
+}
+</style>
