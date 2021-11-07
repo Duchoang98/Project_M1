e2773b40429918c5fc1d41020bd91b5c6708fd86
MerzakYazid <ya.merzak@gmail.com>
2021-11-08 00:32:49 +0100

    little mistake

--- vue-site-e-commerce/src/views/Homepage.vue
+++ vue-site-e-commerce/src/views/Homepage.vue
@@ -7,14 +7,12 @@
 
 <script>
 import ComicRow from "../components/ComicRow.vue";
-import BDD from "../BDD";
 export default {
   name: "Homepage",
   components: {
     ComicRow,
   },
   setup() {
-    console.log(BDD);
   },
 };
 </script>
